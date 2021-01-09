2fed9d8100b187e1464a0e929bd8b947c245d206
David Kavanaugh <davidkavanaugh.dev@outlook.com>
2021-01-09 14:58:13 -0800

    feat: Reach Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React from "react";
+import { Router } from "@reach/router";
 import Search from "./Search";
+import state from "./state";
+import Person from "./Person";
+import Planet from "./Planet";
+import NotFound from "./NotFound";
+
 const App = () => {
+  const {
+    data,
+    setData,
+    getData,
+    selection,
+    updateSelection,
+    id,
+    updateId,
+  } = state();
+
   return (
     <>
-      <Search />
+      <Search
+        selection={selection}
+        updateSelection={updateSelection}
+        id={id}
+        updateId={updateId}
+        path="/"
+      />
+      <Router>
+        <Person
+          data={data}
+          getData={getData}
+          setData={setData}
+          path="/people/:id"
+        />
+        <Planet
+          data={data}
+          getData={getData}
+          setData={setData}
+          path="/planets/:id"
+        />
+        <NotFound path="/error" />
+      </Router>
     </>
   );
 };
