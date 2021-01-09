9085f3bceb13e442b252eca6e1bbda3de3bc8c0b
David Kavanaugh <davidkavanaugh.dev@outlook.com>
2021-01-09 15:02:39 -0800

    feat: axios get

--- src/state.js
+++ src/state.js
@@ -1,36 +1,35 @@
 import { useState } from "react";
 import axios from "axios";
+import NotFoundError from "./NotFoundError";
 
 export default (init = []) => {
   const [data, setData] = useState(init);
   const [selection, setSelection] = useState("");
   const [id, setId] = useState("");
 
-  const getData = (selection, id) => {
-    axios
-      .get(`https://swapi.dev/api/${selection}/${id}`)
-      .then((response) => {
-        setData(response.data);
-        return response.data;
-      })
-      .catch((err) => {
-        throw new Error({
-          error: err,
-          message: "These aren't the droids you're looking for",
-        });
-      });
-  };
-  const updateSelection = (selection) => {
-    console.log(selection);
-    setSelection(selection);
+  const getData = async (selection, id) => {
+    try {
+      if (!id) {
+        throw new NotFoundError("These aren't the droids you're looking for");
+      }
+      let response = await axios.get(
+        `https://swapi.dev/api/${selection}/${id}`
+      );
+      return response;
+    } catch (err) {
+      throw new NotFoundError("These aren't the droids you're looking for");
+    }
   };
 
   const updateId = (id) => {
-    console.log(id);
     setId(id);
+  };
+  const updateSelection = (selection) => {
+    setSelection(selection);
   };
   return {
     data,
+    setData,
     getData,
     selection,
     updateSelection,
